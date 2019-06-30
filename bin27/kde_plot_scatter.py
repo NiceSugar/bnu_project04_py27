d4fadafd4abfc1a9008d6c58389aeda932ca80c6
Yang Li <leeyang1991@gmail.com>
2019-06-30 11:49:20 +0800

    update plot scatter

--- bin27/kde_plot_scatter.py
+++ bin27/kde_plot_scatter.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.stats import gaussian_kde as kde
 import matplotlib as mpl
+import math
 
 
 def reverse_colourmap(cmap, name = 'my_cmap_r'):
@@ -57,11 +58,77 @@ def makeColours(vals,cmap,reverse=0):
         colors.append(cmap(i))
     return colors
 
+def linefit(x, y):
+    '''
+    最小二乘法拟合直线
+    :param x:
+    :param y:
+    :return:
+    '''
+    N = float(len(x))
+    sx, sy, sxx, syy, sxy = 0, 0, 0, 0, 0
+    for i in range(0, int(N)):
+        sx += x[i]
+        sy += y[i]
+        sxx += x[i] * x[i]
+        syy += y[i] * y[i]
+        sxy += x[i] * y[i]
+    a = (sy * sx / N - sxy) / (sx * sx / N - sxx)
+    b = (sy - a * sx) / N
+    r = abs(sy * sx / N - sxy) / math.sqrt((sxx - sx * sx / N) * (syy - sy * sy / N))
+    return a, b, r
 
 
 
-def plot_scatter(val1,val2,cmap='RdYlBu',reverse=1,s=1.,title = ''):
+def plot_fit_line( a, b, r, X, Y, title=''):
+    '''
+    画拟合直线 y=ax+b
+    画散点图 X,Y
+    :param a:
+    :param b:
+    :param X:
+    :param Y:
+    :param i:
+    :param title:
+    :return:
+    '''
+    x = np.linspace(min(X), max(X), 10)
+    y = a * x + b
+    #
+    # plt.subplot(2,2,i)
+    # plt.scatter(X,Y,marker='o',s=5,c = 'grey')
+    # plt.plot(X, Y)
+    plt.plot(x, y, linestyle='dashed', c='black', linewidth=2, alpha=0.7)
+    plt.title(title)
 
+
+
+def plot_scatter(val1,val2,cmap='Spectral',reverse=1,s=1.):
+    plt.figure(figsize=(6,6))
+    x = [-1e10,1e10]
+    y = [-1e10,1e10]
+    plt.plot(x,y,c='r')
+
+    max_x = np.max(val1)
+    max_y = np.max(val2)
+    maxi = max([max_x,max_y])
+
+    min_x = np.min(val1)
+    min_y = np.min(val2)
+    mini = min([min_x,min_y])
+
+
+    length = len(val1)
+    print('origin len(val):%s'%length)
+    if length > 15000:
+        n = len(val1)/15000
+    else:
+        n = 1
+    # print(n)
+    n = int(n)
+    val1 = val1[::n]
+    val2 = val2[::n]
+    print('KDE len:%s'%len(val1))
     kde_val = np.array([val1,val2])
     print('doing kernel density estimation... ')
     densObj = kde(kde_val)
@@ -76,15 +143,18 @@ def plot_scatter(val1,val2,cmap='RdYlBu',reverse=1,s=1.,title = ''):
     # set background color
     # ax = plt.gca()
     # ax.set_facecolor('black')
-    plt.figure()
+
     if reverse:
-        # plt.title(cmap+'_reverse')
-        plt.title(title)
+        plt.title(cmap+'_reverse')
     else:
-        plt.title(title)
+        plt.title(cmap)
 
     plt.scatter(val1,val2,c=colors,s=s)
+    plt.xlim(mini, maxi)
+    plt.ylim(mini, maxi)
     # plt.xlim((0.17, 0.30))
     # plt.ylim((0.11, 0.30))
+    a, b, r = linefit(val1,val2)
+    plot_fit_line(a,b,r,val1,val2)
     print('showing...')
     # plt.show()
